894489ebdfe4472859aa572e68c2912b0b028d00
Navdeep Sharma <navdeepsharma4072@gmail.com>
2023-04-09 22:45:47 +0530

    final1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
-import About from "./components/About";
-import { BrowserRouter as Router, Routes, Route,Link, BrowserRouter } from "react-router-dom";
+// import About from "./components/About";
+// import { BrowserRouter as Router, Routes, Route,Link, BrowserRouter } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -34,24 +34,26 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
       <Navbar title="Text Analyzer" mode={mode} toggleMode={toggleMode} />
 
       <Alert alert={alert} />
       
       <div className="container my-3">
         
-          <Routes>
-            <Route exact path="/about" element={<About />} />
-            <Route exact path="/" element={<TextForm
+          {/* <Routes> */}
+            {/* <Route exact path="/about" element={<About />} /> */}
+            {/* <Route exact path="/" element={ */}
+            <TextForm
                 heading="ENTER YOUR TEXT BELOW TO ANALYZE"
                 mode={mode}
                 showAlert={showAlert}
-              />} />
-          </Routes>
+              />
+              {/* } /> */}
+          {/* </Routes> */}
           
       </div>
-      </BrowserRouter>
+      {/* </BrowserRouter> */}
       </> 
   );
 }
